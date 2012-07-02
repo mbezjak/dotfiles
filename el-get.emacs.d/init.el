97e9dc149a4ae316977b2913997672b874bd8bb5
Miro Bezjak <bezjak.miro@gmail.com>
2012-07-02 23:30:38 +0200

    Add marmalade package repository.

--- el-get.emacs.d/init.el
+++ el-get.emacs.d/init.el
@@ -1,3 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
 ;; disable mouse interface
 (menu-bar-mode -1)
 (tool-bar-mode -1)
