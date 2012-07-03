e9d388a3ee0a6d66d272d77d555f7612b45101e0
Miro Bezjak <bezjak.miro@gmail.com>
2012-07-03 16:15:38 +0200

    Set `custom-file' to `~/.emacs.d/custom.el'.

--- el-get.emacs.d/init.el
+++ el-get.emacs.d/init.el
@@ -3,6 +3,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (column-number-mode t)
 (setq tab-width 4)
 
