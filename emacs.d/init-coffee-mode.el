d64631752c0fce1651ed451408e4f6aedbc5fca4
Miro Bezjak <bezjak.miro@gmail.com>
2012-07-16 12:33:53 +0200

    Haskell and coffee modes run 'prog-mode-hook.

--- emacs.d/init-coffee-mode.el
+++ emacs.d/init-coffee-mode.el
@@ -1,1 +1,2 @@
+(add-hook 'coffee-mode-hook 'esk-prog-mode-hook)
 (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
--- emacs.d/init-haskell-mode.el
+++ emacs.d/init-haskell-mode.el
@@ -1,3 +1,4 @@
+(add-hook 'haskell-mode-hook 'esk-prog-mode-hook)
 (add-hook 'haskell-mode-hook
 	  (lambda ()
 	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
