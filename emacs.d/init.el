66925d583a8f7efad9860c91bdc456e9a47cb71a
Miro Bezjak <bezjak.miro@gmail.com>
2012-08-18 10:35:23 +0200

    Move `~/.recentf' to emacs directory.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,7 +12,8 @@
               c-basic-offset 4
               fill-column 80)
 
-(setq ido-save-directory-list-file (concat user-emacs-directory ".ido.last"))
+(setq ido-save-directory-list-file (concat user-emacs-directory ".ido.last")
+      recentf-save-file (concat user-emacs-directory ".recentf"))
 
 (defun find-org-files ()
   (interactive)
