8ab66e2eacd74ca9c977b0883126b5a49677a35b
Miro Bezjak <bezjak.miro@gmail.com>
2012-09-24 17:48:19 +0200

    Foreground face for diff-{add,del} is the same as in magit.

--- emacs.d/init-monky.el
+++ emacs.d/init-monky.el
@@ -1,2 +1,7 @@
+(require 'monky)
+
+(set-face-foreground 'monky-diff-add "green4")
+(set-face-foreground 'monky-diff-del "red3")
+
 (setq monky-process-type 'cmdserver)
 (global-set-key (kbd "C-c h") 'monky-status)
