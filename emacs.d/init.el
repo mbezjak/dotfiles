9194b64cdda308080d29e2f624ea92288f6276ee
Miro Bezjak <bezjak.miro@gmail.com>
2012-10-11 10:47:28 +0200

    Split package configuration to my-packages.el.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -76,51 +76,5 @@
 (require 'gommons-mode)
 (require 'mm-url) ; to include mm-url-decode-entities-string
 
-;; el-get.el dependent code
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil t)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-      (let (el-get-master-branch)
-        (goto-char (point-max))
-        (eval-print-last-sexp))))
-
-(setq el-get-user-package-directory user-emacs-directory)
-
-;; Everything above `starter-kit' are its dependencies.
-;; It seems they are needed for proper load order.
-(setq el-get-sources
-      '((:name paredit              :type elpa)
-        (:name idle-highlight-mode  :type elpa)
-        (:name find-file-in-project :type elpa)
-        (:name smex                 :type elpa)
-        (:name ido-ubiquitous       :type elpa)
-        (:name magit                :type elpa)
-        (:name monky                :type elpa)
-        (:name starter-kit          :type elpa)
-        (:name starter-kit-bindings :type elpa)
-        (:name starter-kit-eshell   :type elpa)
-        (:name starter-kit-lisp     :type elpa)
-        (:name haskell-mode         :type elpa)
-        (:name coffee-mode          :type elpa)
-        (:name groovy-mode          :type elpa)
-        (:name scala-mode           :type elpa)
-        (:name markdown-mode        :type elpa)
-        (:name yasnippet            :type elpa)
-        (:name switch-window        :type elpa)
-        (:name projectile           :type elpa)
-        (:name htmlize              :type elpa)
-        (:name ensime :load-path ("./dist_2.9.2/elisp"))))
-
-(setq my-el-get-elpa-packages
-      (mapcar 'el-get-source-name el-get-sources))
-
-(setq my-el-get-packages
-      '(ghc-mod))
-
-(setq my-packages
-      (append my-el-get-elpa-packages my-el-get-packages))
-
-(el-get 'sync my-packages)
+;; el-get and melpa package initialization
+(require 'my-packages)
