644ce025c35b7460df51c3640f9c955b76208501
Miro Bezjak <bezjak.miro@gmail.com>
2012-10-23 16:06:05 +0200

    Break apart all org templates to be more readable.

--- emacs.d/my-org.el
+++ emacs.d/my-org.el
@@ -11,19 +11,42 @@
       org-refile-targets '((nil :maxlevel . 3)
                            (org-agenda-files :maxlevel . 2)))
 
-(setq org-capture-templates
- '(("t" "Todo" entry (file+headline "pool.org" "SHORT") "* %?")
-   ("N" "Stub Next Week" entry (file+headline "agenda.org" "WEEKLY PLEDGE")
-    "* TODO Fallacy x1
+
+;; org-capture-templates
+(setq my-org-capture-template-todo
+      '("t" "Todo" entry (file+headline "pool.org" "SHORT")
+        "* %?"))
+
+(setq my-org-capture-template-work-todo
+      '("w" "Work Todo" entry (file+headline "work.org" "GENERAL")
+        "* %?"))
+
+(setq my-org-capture-template-to-read
+      '("r" "To Read" entry (file+headline "pool.org" "TO READ")
+        "* %?"))
+
+(setq my-org-capture-template-next-week
+      '("N" "Next Week Tasks" entry (file+headline "agenda.org" "WEEKLY PLEDGE")
+        "* TODO Fallacy x1
 * TODO KhanAcademy video x1
 * TODO KhanAcademy video x2
 * TODO KhanAcademy practice x1
-* TODO KhanAcademy practice x2")
-   ("w" "Work Todo" entry (file+headline "work.org" "GENERAL") "* %?")
-   ("P" "Pressure" table-line (file+headline "everything.org" "PRESSURE")
-    "| %U | %? |   |   |" :table-line-pos "II-1")
-   ("r" "To Read" entry (file+headline "pool.org" "TO READ") "* %?")))
+* TODO KhanAcademy practice x2"))
 
+(setq my-org-capture-template-pressure
+      '("P" "Pressure" table-line (file+headline "everything.org" "PRESSURE")
+        "| %U | %? |   |   |"
+        :table-line-pos "II-1"))
+
+(setq org-capture-templates
+      (list my-org-capture-template-todo
+            my-org-capture-template-work-todo
+            my-org-capture-template-to-read
+            my-org-capture-template-next-week
+            my-org-capture-template-pressure))
+
+
+;; functions
 (defun my-org-unbind-keys ()
   "Unset keys that are not useful in org-mode."
   (local-unset-key (kbd "C-c ["))
