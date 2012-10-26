2a5dd44259821acf13c5ed6d3b631b7a40556e69
Miro Bezjak <bezjak.miro@gmail.com>
2012-10-26 21:52:16 +0200

    org-capture-templates are provided in a separate file.

--- emacs.d/my-org.el
+++ emacs.d/my-org.el
@@ -11,39 +11,8 @@
       org-refile-targets '((nil :maxlevel . 3)
                            (org-agenda-files :maxlevel . 2)))
 
-
-;; org-capture-templates
-(setq my-org-capture-template-todo
-      '("t" "Todo" entry (file+headline "pool.org" "SHORT")
-        "* %?"))
-
-(setq my-org-capture-template-work-todo
-      '("w" "Work Todo" entry (file+headline "work.org" "GENERAL")
-        "* %?"))
-
-(setq my-org-capture-template-to-read
-      '("r" "To Read" entry (file+headline "pool.org" "TO READ")
-        "* %?"))
-
-(setq my-org-capture-template-next-week
-      '("N" "Next Week's Floaters" entry (file+headline "agenda.org" "WEEKLY PLEDGE")
-        "* TODO [[http://www.nizkor.org/features/fallacies/][Fallacy]] x1 +take-home-msg
-* TODO KhanAcademy video x1
-* TODO KhanAcademy video x2
-* TODO KhanAcademy practice x1
-* TODO KhanAcademy practice x2"))
-
-(setq my-org-capture-template-pressure
-      '("p" "Pressure" table-line (file+headline "everything.org" "PRESSURE")
-        "| %U | %? |   |   |"
-        :table-line-pos "II-1"))
-
-(setq org-capture-templates
-      (list my-org-capture-template-todo
-            my-org-capture-template-work-todo
-            my-org-capture-template-to-read
-            my-org-capture-template-next-week
-            my-org-capture-template-pressure))
+;; provides org-capture-templates
+(load (concat org-directory "/capture-templates"))
 
 
 ;; functions
