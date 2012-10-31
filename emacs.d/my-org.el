6743832897e94662ffd615fe4e2ccc37a2e04689
Miro Bezjak <bezjak.miro@gmail.com>
2012-10-31 11:04:49 +0100

    ~/Dropbox/notes/org-custom.el provides org-agenda-custom-commands.

--- emacs.d/my-org.el
+++ emacs.d/my-org.el
@@ -11,8 +11,8 @@
       org-refile-targets '((nil :maxlevel . 3)
                            (org-agenda-files :maxlevel . 2)))
 
-;; provides org-capture-templates
-(load (concat org-directory "/capture-templates"))
+;; org-agenda-custom-commands and org-capture-templates
+(load (concat org-directory "/org-custom"))
 
 
 ;; functions
