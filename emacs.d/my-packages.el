ca0031e56ef98c37b7751d61dc01cb07b3acb5b1
Miro Bezjak <bezjak.miro@gmail.com>
2012-11-02 15:28:53 +0100

    Update el-get installation procedure.
    Install procedure is the same as the one suggested on el-get homepage.

--- emacs.d/my-packages.el
+++ emacs.d/my-packages.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil t)
+(unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
