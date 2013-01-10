615c658e436b238f38cd8fa47a1717d6fc0695da
Miro Bezjak <bezjak.miro@gmail.com>
2013-01-10 10:54:00 +0100

    Disable suspend-frame (key C-x C-z).

--- emacs.d/my-keys.el
+++ emacs.d/my-keys.el
@@ -1,5 +1,6 @@
-(global-unset-key (kbd "C-x C-c")) ; shutdown with alt + F4
-(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-c")) ; save-buffers-kill-terminal; shutdown with alt + F4
+(global-unset-key (kbd "C-z"))     ; suspend-frame
+(global-unset-key (kbd "C-x C-z")) ; suspend-frame
 
 ;; jump to directory
 (defmacro my-jump-macro (fnname dir)
