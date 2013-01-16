efb02299c26cdda181d19a98f98ddd9b6b3ec818
Miro Bezjak <bezjak.miro@gmail.com>
2013-01-16 15:04:33 +0100

    Some emacs checkdoc fixes.

--- emacs.d/gommons-mode.el
+++ emacs.d/gommons-mode.el
@@ -25,65 +25,65 @@
 
 ;; PREDICATES
 (defun gommons-coffee-src-p (name)
-  "Test if NAME is a coffeescript source file"
+  "Test if NAME is a coffeescript source file."
   (string-match-p gommons-rel-coffee-src name))
 
 (defun gommons-coffee-test-p (name)
-  "Test if NAME is a coffeescript test file"
+  "Test if NAME is a coffeescript test file."
   (string-match-p gommons-rel-coffee-test name))
 
 (defun gommons-js-src-p (name)
-  "Test if NAME is a javascript source file"
+  "Test if NAME is a javascript source file."
   (string-match-p gommons-rel-js-src name))
 
 (defun gommons-js-test-p (name)
-  "Test if NAME is a javascript test file"
+  "Test if NAME is a javascript test file."
   (string-match-p gommons-rel-js-test name))
 
 
 ;; COMBINED PREDICATES
 (defun gommons-src-p (name)
-  "Test if NAME is a source file"
+  "Test if NAME is a source file."
   (or (gommons-coffee-src-p name)
       (gommons-js-src-p     name)))
 
 (defun gommons-test-p (name)
-  "Test if NAME is a test file"
+  "Test if NAME is a test file."
   (or (gommons-coffee-test-p name)
       (gommons-js-test-p     name)))
 
 
 ;; FUNCTIONS
 (defun gommons-rel-file (name)
-  "Relative file to `gommons-root-dir'"
+  "Relative file to `gommons-root-dir'."
   (file-relative-name name gommons-root-dir))
 
 (defun gommons-new-ext (ext name)
-  "Return filename with given file extension"
+  "Return filename with given file extension."
   (concat (file-name-sans-extension name) "." ext))
 
 (defun gommons-msg-with-name (tpl name)
-  "Send TPL message and replace '%s' with NAME"
+  "Send TPL message and replace '%s' with NAME."
   (let ((rel (gommons-rel-file name)))
     (message (format tpl rel))))
 
 (defun gommons-open-file (name)
-  "Open NAME in new buffer"
+  "Open NAME in new buffer."
   (if (file-exists-p name)
       (find-file name)
     (gommons-msg-with-name "File '%s' does not exist!" name)))
 
 (defun gommons-browse-jsdoc ()
-  "Browse javascript documentation"
+  "Browse javascript documentation."
   (browse-url gommons-jsdoc-file))
 
 (defun gommons-browse-manual (name)
-  "Browse reference manual"
+  "Browse reference manual."
   (let ((file (format gommons-refman-file name)))
     (when (file-exists-p file) (browse-url file))))
 
 (defun gommons-exec (basedir cmd &optional onfinish)
-  "Execute shell CMD in BASEDIR as working directory"
+  "Execute shell CMD in BASEDIR as working directory."
   (setq gommons-referring-buffer (buffer-name))
 
   (let* ((buffer-name "*gommons*")
@@ -100,39 +100,39 @@
 
 ;; FROM-TO FUNCTIONS
 (defun gommons-name-replace (from to name)
-  "Replace FROM to TO in a name"
+  "Replace FROM to TO in a name."
   (replace-regexp-in-string from to name t t))
 
 (defun gommons-coffee-from-src-to-test (name)
-  "Return coffeescript test file for given src file"
+  "Return coffeescript test file for given src file."
   (gommons-name-replace gommons-rel-coffee-src gommons-rel-coffee-test name))
 
 (defun gommons-coffee-from-test-to-src (name)
-  "Return coffeescript src file for given test file"
+  "Return coffeescript src file for given test file."
   (gommons-name-replace gommons-rel-coffee-test gommons-rel-coffee-src name))
 
 (defun gommons-js-from-src-to-test (name)
-  "Return javascript test file for given src file"
+  "Return javascript test file for given src file."
   (gommons-new-ext "jspec"
    (gommons-name-replace gommons-rel-js-src gommons-rel-js-test name)))
 
 (defun gommons-js-from-test-to-src (name)
-  "Return javascript src file for given test file"
+  "Return javascript src file for given test file."
   (gommons-new-ext "js"
    (gommons-name-replace gommons-rel-js-test gommons-rel-js-src name)))
 
 (defun gommons-from-src-to-test (name)
-  "Return test file for given src file"
+  "Return test file for given src file."
   (cond ((gommons-coffee-src-p name) (gommons-coffee-from-src-to-test name))
         ((gommons-js-src-p name)     (gommons-js-from-src-to-test name))))
 
 (defun gommons-from-test-to-src (name)
-  "Return src file for given test file"
+  "Return src file for given test file."
   (cond ((gommons-coffee-test-p name) (gommons-coffee-from-test-to-src name))
         ((gommons-js-test-p name)     (gommons-js-from-test-to-src name))))
 
 (defun gommons-toggle-open-src-test (name)
-  "Jump between src and test file"
+  "Jump between src and test file."
   (cond
    ((gommons-src-p name) (gommons-open-file (gommons-from-src-to-test name)))
    ((gommons-test-p name) (gommons-open-file (gommons-from-test-to-src name)))
@@ -141,28 +141,28 @@
 
 ;; COMMANDS
 (defun gommons-toggle-open-current ()
-  "Interactive `gommons-toggle-open-src-test' for current buffer name"
+  "Interactive `gommons-toggle-open-src-test' for current buffer name."
   (interactive)
   (gommons-toggle-open-src-test (buffer-file-name)))
 
 (defun gommons-test-js ()
-  "Run coffeescript and javascript tests"
+  "Run coffeescript and javascript tests."
   (interactive)
   (gommons-exec gommons-js-dir gommons-test-js-cmd))
 
 (defun gommons-test-unit ()
-  "Run grails unit tests"
+  "Run grails unit tests."
   (interactive)
   (gommons-exec gommons-root-dir gommons-test-unit-cmd))
 
 (defun gommons-docs-js ()
-  "Create javascript documentation"
+  "Create javascript documentation."
   (interactive)
   (gommons-exec gommons-js-dir gommons-docs-js-cmd
                 (lambda (buffer status) (gommons-browse-jsdoc))))
 
 (defun gommons-docs-manual ()
-  "Create reference manual"
+  "Create reference manual."
   (interactive)
   (gommons-exec gommons-refman-dir gommons-docs-refman-cmd
      (lambda (buffer status)
@@ -170,7 +170,7 @@
         (file-name-sans-extension gommons-referring-buffer)))))
 
 (defun gommons-release ()
-  "Publish new version of the plugin"
+  "Publish new version of the plugin."
   (interactive)
   (when (yes-or-no-p "Confirm plugin release? ")
     (gommons-exec gommons-root-dir gommons-release-cmd)))
