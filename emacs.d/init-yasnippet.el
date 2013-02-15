823ebb5b4300cf21db72caef93f4630213f058c1
Miro Bezjak <bezjak.miro@gmail.com>
2013-02-15 23:43:32 +0100

    Function that outputs package from a buffer-file-name.

--- emacs.d/init-yasnippet.el
+++ emacs.d/init-yasnippet.el
@@ -5,3 +5,17 @@
 (defun my-yas-buffer-class-name ()
   (let ((file (or (buffer-file-name) "Foo")))
     (file-name-nondirectory (file-name-sans-extension file))))
+
+(defun my-yas-buffer-package-name ()
+  (let* ((default-pkg "foo")
+         (fullpath (or (buffer-file-name) default-pkg))
+         (dirs (or (file-name-directory fullpath) default-pkg))
+         (replacements '(".*/src/\\(main\\|test\\)/\\(java\\|groovy\\)"
+                         ".*/src/groovy"
+                         ".*/test/unit"
+                         ".*/app"
+                         "^/"
+                         "/$"))
+         (reduction (lambda (path regex) (replace-regexp-in-string regex "" path)))
+         (pkg (-reduce-from reduction dirs replacements)))
+    (replace-regexp-in-string "/" "." pkg)))
