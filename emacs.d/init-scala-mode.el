fe1801f00a0ffbaa28c27c505abd2aee6f7e4711
Miro Bezjak <bezjak.miro@gmail.com>
2013-02-15 10:16:32 +0100

    scala-mode: C-c C-l starts interpreter if it is not running then loads the buffer.

--- emacs.d/init-scala-mode.el
+++ emacs.d/init-scala-mode.el
@@ -2,4 +2,11 @@
 
 (eval-after-load "scala-mode"
   '(progn
-     (define-key scala-mode-map (kbd "C-c l") 'scala-load-file)))
+     (define-key scala-mode-map (kbd "C-c l")   'my-scala-load-file)
+     (define-key scala-mode-map (kbd "C-c C-l") 'my-scala-load-file)))
+
+(defun my-scala-load-file ()
+  (interactive)
+  (if (scala-interpreter-running-p-1)
+      (scala-load-file (buffer-file-name))
+    (scala-run-scala "scala")))
