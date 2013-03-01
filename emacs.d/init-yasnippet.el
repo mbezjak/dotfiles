f4f10e84e59720d8d99b1349020c24cb48ec372f
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-01 12:39:52 +0100

    More restrictive regexp when extracting package name.

--- emacs.d/init-yasnippet.el
+++ emacs.d/init-yasnippet.el
@@ -10,12 +10,12 @@
   (let* ((default-pkg "foo")
          (fullpath (or (buffer-file-name) default-pkg))
          (dirs (or (file-name-directory fullpath) default-pkg))
-         (replacements '(".*/src/\\(main\\|test\\)/\\(java\\|groovy\\)"
-                         ".*/src/groovy"
-                         ".*/test/unit"
-                         ".*/app"
+         (replacements '(".*/src/\\(main\\|test\\)/\\(java\\|groovy\\)/"
+                         ".*/src/groovy/"
+                         ".*/test/unit/"
+                         ".*/app/"
                          "^/"
                          "/$"))
-         (reduction (lambda (path regex) (replace-regexp-in-string regex "" path)))
+         (reduction (lambda (path regex) (replace-regexp-in-string regex "" path t)))
          (pkg (-reduce-from reduction dirs replacements)))
     (replace-regexp-in-string "/" "." pkg)))
