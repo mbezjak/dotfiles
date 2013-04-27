e8388c6a19475955cd11a925ddd1c02f2a9e30ba
Miro Bezjak <bezjak.miro@gmail.com>
2013-04-27 19:21:01 +0200

    Experimental keys for org-agenda, find-file and switch-buffer.

--- emacs.d/my-keys.el
+++ emacs.d/my-keys.el
@@ -22,18 +22,21 @@
 
 ;; org-mode keys
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "<f5>")  'org-agenda)
 (global-set-key (kbd "<f4>")  'org-capture)
 
 ;; rest
 (global-set-key (kbd "M-<f1>")  'kmacro-start-macro-or-insert-counter)
 (global-set-key (kbd "<f1>")    'kmacro-end-or-call-macro)
-(global-set-key (kbd "M-<f8>")  'point-to-register)
-(global-set-key (kbd "<f8>")    'jump-to-register)
+(global-set-key (kbd "M-<f6>")  'point-to-register)
+(global-set-key (kbd "<f6>")    'jump-to-register)
+(global-set-key (kbd "<f8>")    'ido-find-file)
 (global-set-key (kbd "M-<f9>")  'previous-buffer)
 (global-set-key (kbd "M-<f10>") 'next-buffer)
 (global-set-key (kbd "C-x C-d") 'ido-dired)
 (global-set-key (kbd "C-x \\")  'align-regexp)
 (global-set-key (kbd "M-K")     'my-kill-current-buffer)
+(global-set-key (kbd "M-<f8>")  'ido-switch-buffer)
 (global-set-key (kbd "M-B")     'ido-switch-buffer)
 (global-set-key (kbd "M-L")     'browse-url)
 (global-set-key (kbd "M-P")     'browse-url-at-point)
