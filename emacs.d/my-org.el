c5ef064bd56d126ac0b3b8478215858658c0e573
Miro Bezjak <bezjak.miro@gmail.com>
2013-10-29 11:56:39 +0100

    Use ido completion in orgmode.

--- emacs.d/my-org.el
+++ emacs.d/my-org.el
@@ -26,6 +26,7 @@
       org-latex-preview-ltxpng-directory (my-managed-file "ltxpng/")
       org-id-locations-file (my-managed-file "org-id-locations")
       org-extend-today-until 5
+      org-completion-use-ido t
       org-refile-targets '((nil :maxlevel . 1)
                            (my-org-note-agenda :maxlevel . 2)
                            (my-org-note-pool   :maxlevel . 1)))
