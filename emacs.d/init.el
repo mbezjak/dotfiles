a5efce39ad2d924aa3c8674881b95985a9eded47
Miro Bezjak <bezjak.miro@gmail.com>
2013-10-30 10:14:21 +0100

    Return previously deleted foreground face for monky-diff.
    This way it's more similar to magit even though it doesn't have the
    item-highlight face.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -78,6 +78,8 @@
 
 
 (require 'monky)
+(set-face-foreground 'monky-diff-add  "green4")
+(set-face-foreground 'monky-diff-del  "red3")
 (setq monky-process-type 'cmdserver)
 (global-set-key (kbd "C-c h") 'monky-status)
 
