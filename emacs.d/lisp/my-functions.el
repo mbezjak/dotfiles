db48fdc49c6e45643d23197efb494c445b63687e
Miro Bezjak <bezjak.miro@gmail.com>
2015-01-25 00:01:35 +0100

    emacs.d: more understandable code

--- emacs.d/lisp/my-functions.el
+++ emacs.d/lisp/my-functions.el
@@ -14,7 +14,8 @@
 
 (defun my-find-file-in-dir (dir)
   "Find file in DIR. Don't use filename or url at point."
-  (helm-find-files-1 (expand-file-name dir)))
+  (let ((pattern (f-full dir)))
+    (helm-find-files-1 pattern)))
 
 (defun my-kill-current-buffer ()
   "Kill current buffer."
