79f1df62a7ce6e4b71fc3123b24fe4dfd0a75d92
Miro Bezjak <bezjak.miro@gmail.com>
2015-02-07 23:41:25 +0100

    emacs.d: remove unneeded keybinding

--- emacs.d/lisp/init.el
+++ emacs.d/lisp/init.el
@@ -81,7 +81,6 @@
 (set-face-foreground 'monky-diff-add  "green4")
 (set-face-foreground 'monky-diff-del  "red3")
 (setq monky-process-type 'cmdserver)
-(global-set-key (kbd "C-c h") 'monky-status)
 
 
 (eval-after-load "magit"
