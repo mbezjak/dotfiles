dbc9be59717254296a89bdf1b25704887ec7a7c7
Miro Bezjak <bezjak.miro@gmail.com>
2015-03-24 15:18:45 +0100

    my-keys.el: fix typo that had no effect

--- emacs.d/lisp/my-keys.el
+++ emacs.d/lisp/my-keys.el
@@ -65,7 +65,7 @@
 
 ;; use regex search by default
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
