ff5ebea8fd0f10e3d6c3d9d80b389801d5dc2099
Miro Bezjak <bezjak.miro@gmail.com>
2015-03-24 15:19:04 +0100

    my-keys.el: experimental keybinding - C-/ query-replace-regexp

--- emacs.d/lisp/my-keys.el
+++ emacs.d/lisp/my-keys.el
@@ -67,6 +67,7 @@
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
+(global-set-key (kbd "C-/") 'query-replace-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-M-%") 'query-replace)
