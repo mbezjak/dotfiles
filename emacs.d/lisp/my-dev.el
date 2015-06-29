4934901f6860816211869487adcd4f6bc8b590c6
Miro Bezjak <bezjak.miro@gmail.com>
2015-06-29 10:42:36 +0200

    emacs.d: install json-mode

--- emacs.d/lisp/my-dev.el
+++ emacs.d/lisp/my-dev.el
@@ -1,9 +1,8 @@
 (eval-when-compile
   (require 'saveplace))
 
-(add-to-list 'auto-mode-alist '("\\.m\\'"    . octave-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.m\\'"  . octave-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 
 (defun my-delete-trailing-whitespace-before-save ()
