3bb080d37b0181770b3f77dd19cae3d5e77e6cdb
Miro Bezjak <bezjak.miro@gmail.com>
2016-03-08 16:05:19 +0100

    emacs.d: use bind-key, unbind-key
    That way, M-x describe-personal-keybindings RET will show what was
    assigned, what was reassigned and what was unassigned.

--- emacs.d/lisp/my-keys.el
+++ emacs.d/lisp/my-keys.el
@@ -1,25 +1,20 @@
-;; avoiding mistakes
+;; M-x describe-personal-keybindings RET
+
 (when window-system
-  (global-unset-key (kbd "C-x C-c"))) ; save-buffers-kill-terminal; shutdown with alt + F4
-(global-unset-key (kbd "C-z"))        ; suspend-frame
-(global-unset-key (kbd "C-x C-z"))    ; suspend-frame
-(global-unset-key (kbd "C-t"))        ; transpose-chars
-(global-unset-key (kbd "C-/"))        ; undo; use C-_
-(global-unset-key (kbd "C-x C-f"))    ; replaced by helm-find-files
-(global-unset-key (kbd "C-a"))        ; replaced by my-move-beginning-of-line
-(global-unset-key (kbd "C-d"))        ; replaced by my-delete-line
-(global-unset-key (kbd "<escape>"))
-(global-unset-key (kbd "C-g"))
-
-;; replaced by move-text
-(global-unset-key (kbd "C-M-<up>"))   ; backward-up-list
-(global-unset-key (kbd "C-M-<down>")) ; down-list
-
-;; replaced by helm-occur
-(global-unset-key (kbd "M-o"))
-
-;; replaced by helm-swoop
-(global-unset-key (kbd "M-i"))
+  (unbind-key "C-x C-c"))
+(unbind-key "C-z")
+(unbind-key "C-x C-z")
+(unbind-key "C-t")
+(unbind-key "C-/")
+(unbind-key "C-x C-f")
+(unbind-key "C-a")
+(unbind-key "C-d")
+(unbind-key "<escape>")
+(unbind-key "C-g")
+(unbind-key "C-M-<up>")
+(unbind-key "C-M-<down>")
+(unbind-key "M-o")
+(unbind-key "M-i")
 
 ;; jump to directory
 (defmacro my-jump-macro (fnname dir)
@@ -28,75 +23,75 @@
 
 
 ;; org-mode keys
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "<f9>")  'org-agenda)
-(global-set-key (kbd "<f4>")  'org-capture)
+(bind-key "C-c a" 'org-agenda)
+(bind-key "<f9>"  'org-agenda)
+(bind-key "<f4>"  'org-capture)
 
 ;; rest
-(global-set-key (kbd "M-<f1>")     'kmacro-start-macro-or-insert-counter)
-(global-set-key (kbd "<f1>")       'kmacro-end-or-call-macro)
-(global-set-key (kbd "M-<f6>")     'point-to-register)
-(global-set-key (kbd "<f6>")       'jump-to-register)
-(global-set-key (kbd "<f8>")       'my-repo-status)
-(global-set-key (kbd "M-<f9>")     'previous-buffer)
-(global-set-key (kbd "M-<f10>")    'next-buffer)
-(global-set-key (kbd "<f12> s")    'my-org-agenda-sort)
-(global-set-key (kbd "<f12> d")    'org-decrypt-entry)
-(global-set-key (kbd "<f12> p")    'my-project-version-at-point)
-(global-set-key (kbd "<f12> l")    'my-org-agenda-remove-later)
-(global-set-key (kbd "C-x \\")     'align-regexp)
-(global-set-key (kbd "C-`")        'align-regexp)
-(global-set-key (kbd "C-~")        'my-align-multi)
-(global-set-key (kbd "C-!")        'my-align-spock-table)
-(global-set-key (kbd "C-x C-m")    'shell)
-(global-set-key (kbd "C-]")        'my-jump-to-workspace)
-(global-set-key (kbd "M-K")        'my-kill-current-buffer)
-(global-set-key (kbd "M-I")        'my-org-insert-link)
-(global-set-key (kbd "M-L")        'browse-url)
-(global-set-key (kbd "M-P")        'browse-url-at-point)
-(global-set-key (kbd "C-c SPC")    'avy-goto-word-1)
-(global-set-key (kbd "C-.")        'ace-window)
-(global-set-key (kbd "M-/") '(lambda ()
-                               (interactive)
-                               (if company-mode
-                                   (company-complete)
-                                 (hippie-expand nil))))
+(bind-key "M-<f1>"     'kmacro-start-macro-or-insert-counter)
+(bind-key "<f1>"       'kmacro-end-or-call-macro)
+(bind-key "M-<f6>"     'point-to-register)
+(bind-key "<f6>"       'jump-to-register)
+(bind-key "<f8>"       'my-repo-status)
+(bind-key "M-<f9>"     'previous-buffer)
+(bind-key "M-<f10>"    'next-buffer)
+(bind-key "<f12> s"    'my-org-agenda-sort)
+(bind-key "<f12> d"    'org-decrypt-entry)
+(bind-key "<f12> p"    'my-project-version-at-point)
+(bind-key "<f12> l"    'my-org-agenda-remove-later)
+(bind-key "C-x \\"     'align-regexp)
+(bind-key "C-`"        'align-regexp)
+(bind-key "C-~"        'my-align-multi)
+(bind-key "C-!"        'my-align-spock-table)
+(bind-key "C-x C-m"    'shell)
+(bind-key "C-]"        'my-jump-to-workspace)
+(bind-key "M-K"        'my-kill-current-buffer)
+(bind-key "M-I"        'my-org-insert-link)
+(bind-key "M-L"        'browse-url)
+(bind-key "M-P"        'browse-url-at-point)
+(bind-key "C-c SPC"    'avy-goto-word-1)
+(bind-key "C-."        'ace-window)
+(bind-key "M-/" '(lambda ()
+                   (interactive)
+                   (if company-mode
+                       (company-complete)
+                     (hippie-expand nil))))
 
 ;; toggle between back-to-indentation and move-beginning-of-line
-(global-set-key (kbd "C-a") 'my-move-beginning-of-line)
+(bind-key "C-a" 'my-move-beginning-of-line)
 
 ;; delete line and keep column position
-(global-set-key (kbd "C-d") 'my-delete-line)
+(bind-key "C-d" 'my-delete-line)
 
 ;; easier to use
-(global-set-key (kbd "<escape>") 'keyboard-quit)
+(bind-key "<escape>" 'keyboard-quit)
 
 ;; explore new modes
-(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
+(bind-key "C-<f10>" 'menu-bar-mode)
 
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
+(bind-key "C-+" 'text-scale-increase)
+(bind-key "C--" 'text-scale-decrease)
 
-(global-set-key (kbd "C-c y") 'bury-buffer)
-(global-set-key (kbd "C-c r") 'revert-buffer)
+(bind-key "C-c y" 'bury-buffer)
+(bind-key "C-c r" 'revert-buffer)
 
 ;; better than M-S-6
-(global-set-key (kbd "C-c q") 'join-line)
+(bind-key "C-c q" 'join-line)
 
 ;; general cleanup
-(global-set-key (kbd "C-c n") 'my-cleanup-buffer)
+(bind-key "C-c n" 'my-cleanup-buffer)
 
 ;; eval and replace anywhere
-(global-set-key (kbd "C-c e") 'my-eval-and-replace)
+(bind-key "C-c e" 'my-eval-and-replace)
 
 ;; use regex search by default
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "M-%") 'query-replace-regexp)
-(global-set-key (kbd "C-/") 'query-replace-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-(global-set-key (kbd "C-M-%") 'query-replace)
+(bind-key "C-s" 'isearch-forward-regexp)
+(bind-key "C-r" 'isearch-backward-regexp)
+(bind-key "M-%" 'query-replace-regexp)
+(bind-key "C-/" 'query-replace-regexp)
+(bind-key "C-M-s" 'isearch-forward)
+(bind-key "C-M-r" 'isearch-backward)
+(bind-key "C-M-%" 'query-replace)
 
 ;; unrestricted help search
 (define-key 'help-command "a" 'apropos)
