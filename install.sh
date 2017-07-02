2ebfadf97f35e5d68d2e88369ec064af3f1018da
Miro Bezjak <bezjak.miro@gmail.com>
2017-07-02 22:13:19 +0200

    Add configuration for i3

--- install.sh
+++ install.sh
@@ -22,10 +22,12 @@ mkdir --parents --mode=700 ~/Dropbox
 mkdir --parents ~/workspace/{xattic,xforeign}
 mkdir --parents ~/.lein
 mkdir --parents ~/.sbt/0.13/plugins
+mkdir --parents ~/.config/i3
 
 install_link emacs.d .emacs.d
 install_link lein/profiles.clj .lein/profiles.clj
 install_link sbt/0.13/plugins/plugins.sbt .sbt/0.13/plugins/plugins.sbt
+install_link i3/config .config/i3/config
 
 for f in $(find -mindepth 1 -maxdepth 1 -type f \
                 -and -not -name '*.sh'   \
