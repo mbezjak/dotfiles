febe48d0235feca5d659470b8cb2ae3a430d8568
Miro Bezjak <bezjak.miro@gmail.com>
2018-01-29 11:41:58 +0100

    lein/profiles: add pomegranate to add dependencies dynamically

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]}
+
+ ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
+ :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}}
