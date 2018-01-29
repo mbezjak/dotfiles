eecbf4fe146da2e82fe11141ad4736a9f32ca841
Miro Bezjak <bezjak.miro@gmail.com>
2018-01-29 11:43:08 +0100

    lein/profiles: update cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]}
+{:repl {:plugins [[cider/cider-nrepl "0.16.0"]]}
 
  ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
  :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}}
