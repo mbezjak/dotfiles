58f50c9dd4625a8cbcb761a94bfdcfbe41712008
Miro Bezjak <bezjak.miro@gmail.com>
2018-02-16 18:31:57 +0100

    lein/profiles: rm empty line

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,5 @@
 {:repl {:plugins [[cider/cider-nrepl "0.16.0"]]
         :dependencies [[com.cemerick/piggieback "0.2.2"]]}
 
-
  ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
  :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}}
