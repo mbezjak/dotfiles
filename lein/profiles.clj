2aef3b846885c0506f7f7960f75f6e4a3e8dd648
Miro Bezjak <bezjak.miro@gmail.com>
2018-03-08 11:45:34 +0100

    lein/profiles: cider-nrepl is not automatically injected

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.16.0"]]
-        :dependencies [[com.cemerick/piggieback "0.2.2"]]}
+{:repl {:dependencies [[com.cemerick/piggieback "0.2.2"]]}
 
  ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
  :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}}
