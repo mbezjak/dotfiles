aa595983e5109b27044dd161b9652bd63623a5bf
Miro Bezjak <bezjak.miro@gmail.com>
2018-03-19 11:30:01 +0100

    lein/profiles: add lein-pprint plugin to dev

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:repl {:dependencies [[com.cemerick/piggieback "0.2.2"]]}
 
  ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
- :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}}
+ :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]
+       :plugins [[lein-pprint "1.1.2"]]}}
