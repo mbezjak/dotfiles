d8db8008ffa93c2bfdcd852f44e5a339a570f137
Miro Bezjak <bezjak.miro@gmail.com>
2018-03-25 15:56:02 +0200

    emacs: remove unnecessary code

--- emacs.d/lisp/my-functions.el
+++ emacs.d/lisp/my-functions.el
@@ -1,16 +1,3 @@
-(require 'mm-url) ; to include mm-url-decode-entities-string
-
-
-(defun my-get-html-title-from-url (url)
-  "Return content in <title> tag."
-  (let (x1 x2 (download-buffer (url-retrieve-synchronously url)))
-    (with-current-buffer download-buffer
-      (goto-char (point-min))
-      (setq x1 (search-forward "<title>"))
-      (search-forward "</title>")
-      (setq x2 (search-backward "<"))
-      (mm-url-decode-entities-string (buffer-substring-no-properties x1 x2)))))
-
 (defun my-find-file-in-dir (dir)
   "Find file in DIR. Don't use filename or url at point."
   (let ((pattern (f-full dir)))
--- emacs.d/lisp/my-org.el
+++ emacs.d/lisp/my-org.el
@@ -14,41 +14,19 @@
       org-log-done       'time
       org-directory      "~/Dropbox/notes"
       my-org-note-agenda  (my-org-note "agenda")
-      my-org-note-meta    (my-org-note "meta")
       my-org-note-archive (my-org-note "archive/archive")
       org-default-notes-file my-org-note-agenda
       org-agenda-files       (list my-org-note-agenda)
       org-archive-location   (concat my-org-note-archive "::")
       org-global-properties '(("BENEFIT_ALL" . "1 2 3 4 5 6 7 8 0.5 0"))
-      org-habit-show-habits-only-for-today nil
-      org-habit-graph-column 70
-      org-habit-show-done-always-green t
       org-tags-exclude-from-inheritance '("crypt")
       org-crypt-key nil
       org-latex-preview-ltxpng-directory (no-littering-expand-var-file-name "org/ltxpng/")
       org-extend-today-until 5
-      org-refile-targets '((nil :maxlevel . 1)
-                           (my-org-note-meta    :maxlevel . 1)
-                           (my-org-note-agenda  :maxlevel . 1))
-      org-agenda-prefix-format '((agenda . " %i %-10:c%?-12t% s %-8e")
-                                 (timeline . "  % s")
-                                 (todo . " %i %-12:c")
-                                 (tags . " %i %-12:c")
-                                 (search . " %i %-12:c")))
-
-;; https://aur.archlinux.org/packages/plantuml/
-(setq org-plantuml-jar-path "/opt/plantuml/plantuml.jar")
+      org-refile-targets '((nil :maxlevel . 1)))
 
 (with-eval-after-load 'org
-  (add-to-list 'org-modules 'org-habit t)
   (plist-put org-format-latex-options :scale 1.5) ; bigger latex fragment
-
-  (org-babel-do-load-languages 'org-babel-load-languages
-                               '((gnuplot . t)
-                                 (plantuml . t)
-                                 (dot . t)
-                                 (sh . t)
-                                 (emacs-lisp . t)))
 
   (add-hook 'org-mode-hook #'(lambda () (flycheck-mode -1)))
 
@@ -78,39 +56,7 @@
 (load (concat org-directory "/org-custom"))
 
 
-;; kbd-macros
-(fset 'my-actually-i-did-it-yesterday
-   [?\C-s ?< ?2 ?0 ?\C-m ?\C-c ?. ?\C-m ?\C-s ?\\ ?\[ ?2 ?0 ?\C-m ?\C-c ?! ?\C-? ?\C-? ?\C-? ?\C-? ?\C-? ?2 ?3 ?: ?0 ?0 home ?- ?1 ?  ?\C-m ?\C-r ?\\ ?\[ ?\C-m ?\C-k ?\C-y ?\C-s ?L ?A ?S ?T ?\C-m ?\C-c ?\C-r ?\C-a tab ?\C-s ?\\ ?\[ ?2 ?0 ?\C-m ?\C-b ?\C-b ?\C-b S-insert ?\C-k])
-
-(fset 'my-correct-for-after-midnight
-   [?\C-s ?< ?2 ?0 ?\C-m ?\C-s ?\\ ?\[ ?2 ?0 ?\C-m ?\C-c ?! ?\C-? ?\C-? ?\C-? ?\C-? ?\C-? ?2 ?3 ?: ?0 ?0 home ?- ?1 ?  ?\C-m ?\C-r ?\\ ?\[ ?\C-m ?\C-k ?\C-y ?\C-s ?L ?A ?S ?T ?\C-m ?\C-c ?\C-r ?\C-a tab ?\C-s ?\\ ?\[ ?2 ?0 ?\C-m ?\C-b ?\C-b ?\C-b S-insert ?\C-k])
-
-(put 'my-actually-i-did-it-yesterday 'kmacro t)
-
-
 ;; functions
-(defun my-org-agenda-sort ()
-  (interactive)
-  (goto-char (point-min))
-
-  (search-forward-regexp "^\* EXPIRES")
-  (beginning-of-line)
-  (org-sort-entries nil ?t)
-  (org-cycle)
-
-  (search-forward-regexp "^\* DEADLINED")
-  (beginning-of-line)
-  (org-sort-entries nil ?t)
-  (org-cycle)
-
-  (search-forward-regexp "^\* SCHEDULED")
-  (beginning-of-line)
-  (org-sort-entries nil ?t)
-  (org-cycle)
-  (org-cycle)
-
-  (save-buffer))
-
 (defun my-org-unbind-keys ()
   "Unset keys that are not useful in org-mode."
   (unbind-key "C-c [")
@@ -123,13 +69,6 @@
              ("M-p" . forward-word)
              :map org-agenda-keymap
              ("C-s" . org-save-all-org-buffers)))
-
-(defun my-org-insert-link ()
-  "Insert org link where default description is set to html title."
-  (interactive)
-  (let* ((url (read-string "URL: "))
-         (title (my-get-html-title-from-url url)))
-    (org-insert-link nil url title)))
 
 ; http://lists.gnu.org/archive/html/emacs-orgmode/2014-02/msg00088.html
 (defun my-org-ask-clock-out ()
