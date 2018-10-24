c471133b4b1875bb1cf1ae8dd371db7477802311
Miro Bezjak <bezjak.miro@gmail.com>
2018-10-24 14:18:00 +0200

    lein/profiles.clj: trying out polylith

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,4 +3,5 @@
  ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
  :dev {:dependencies [[com.cemerick/pomegranate "1.0.0"]]}
 
- :user {:plugins [[lein-pprint "1.1.2"]]}}
+ :user {:plugins [[lein-pprint "1.1.2"]
+                  [polylith/lein-polylith "0.1.1"]]}}
