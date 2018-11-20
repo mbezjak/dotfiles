b31c5c05ea5a508ee87e2e30bdcda5f6e2e254be
Miro Bezjak <bezjak.miro@gmail.com>
2018-11-20 13:07:22 +0100

    lein/profiles: cljs repl will be reconfigured when needed

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,5 @@
-{:repl {:dependencies [[com.cemerick/piggieback "0.2.2"]]}
-
- ;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
- :dev {:dependencies [[com.cemerick/pomegranate "1.1.0"]]}
+;; https://stackoverflow.com/questions/16409182/any-way-to-add-dependency-to-lein-project-without-repl-restart
+{:dev {:dependencies [[com.cemerick/pomegranate "1.1.0"]]}
 
  :user {:plugins [[lein-pprint "1.2.0"]
                   [polylith/lein-polylith "0.2.0"]]}}
