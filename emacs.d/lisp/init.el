28c7b89a38a08b410b2a99e49d68ab6ef91ed72c
Miro Bezjak <bezjak.miro@gmail.com>
2018-12-11 11:02:18 +0100

    emacs: I don't think this is necessary

--- emacs.d/lisp/init.el
+++ emacs.d/lisp/init.el
@@ -66,9 +66,7 @@
 
 (use-package no-littering
   :ensure t
-  :config
-  (setq auto-save-file-name-transforms
-        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+  :config)
 
 (use-package recentf
   :init
