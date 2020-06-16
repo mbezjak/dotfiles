b45ad9c983b6c05fe9a7cd94f347ab4c9b4f55b0
Miro Bezjak <bezjak.miro@gmail.com>
2020-06-16 12:42:47 +0200

    emacs: update epla

---
+++ emacs.d/elpa/crux-20200616.543/crux-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crux" "20200616.543" "A Collection of Ridiculously Useful eXtensions" '((seq "1.11")) :stars nil :commit "2e16b828910c9b8acba37e712d21b517d2cf78cc" :keywords '("convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/crux")
--- emacs.d/elpa/org-20200608/org-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "org"
-  "20200608" "Outline-based notes management and organizer" ( ))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
