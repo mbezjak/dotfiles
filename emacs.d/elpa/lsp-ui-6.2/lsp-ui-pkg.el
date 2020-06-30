3a2efe2f5e8609de65d81025460c6140ac4929d9
Miro Bezjak <bezjak.miro@gmail.com>
2020-06-30 15:12:20 +0200

    emacs: update elpa

---
+++ emacs.d/elpa/lsp-ui-6.2/lsp-ui-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lsp-ui" "6.2" "UI modules for lsp-mode"
+  '((emacs "25.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "207bc8d61abff7c3024f8a484f63bc60bc4ad58b" :keywords
+  '("lsp")
+  :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/org-20200629/org-pkg.el
@@ -0,0 +1,5 @@
+(define-package "org"
+  "20200629" "Outline-based notes management and organizer" ( ))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
