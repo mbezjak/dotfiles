59051e62cc132d21b5b738a2dc746e1c9e5ecbd9
Miro Bezjak <bezjak.miro@gmail.com>
2020-09-02 17:34:45 +0200

    emacs: add function that takes SVG screenshot of emacs

--- emacs.d/lisp/my-functions.el
+++ emacs.d/lisp/my-functions.el
@@ -141,6 +141,18 @@ Same as `replace-string C-q C-m RET RET'."
   (my-rename-file-and-buffer (concat new-name "." (f-ext (buffer-file-name))))
   (my-coffee-change-fn-name-to new-name))
 
+;; https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 (defun my-pretty-lambdas ()
   (font-lock-add-keywords
    nil `(("(?\\(lambda\\>\\)"
