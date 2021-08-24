ee333e9c293f41bc5e0676d6c1ca579d1c56d589
Miro Bezjak <bezjak.miro@gmail.com>
2021-08-24 18:49:39 +0200

    emacs: use package auto-dim-other-buffers

--- emacs.d/lisp/init.el
+++ emacs.d/lisp/init.el
@@ -750,18 +750,22 @@
 (use-package terraform-mode
   :ensure t)
 
-;(use-package lsp-mode
-;  :ensure t
-;  :pin melpa
-;  :hook (groovy-mode . lsp)
-;  :commands lsp)
-;
-;(use-package lsp-ui
-;  :ensure t
-;  :commands lsp-ui-mode)
-;(use-package company-lsp
-;  :ensure t
-;  :commands company-lsp)
-;(use-package helm-lsp
-;  :ensure t
-;  :commands helm-lsp-workspace-symbol)
+(use-package auto-dim-other-buffers
+  :ensure t
+  :hook ((after-init . auto-dim-other-buffers-mode)))
+
+;;(use-package lsp-mode
+;;  :ensure t
+;;  :pin melpa
+;;  :hook (groovy-mode . lsp)
+;;  :commands lsp)
+;;
+;;(use-package lsp-ui
+;;  :ensure t
+;;  :commands lsp-ui-mode)
+;;(use-package company-lsp
+;;  :ensure t
+;;  :commands company-lsp)
+;;(use-package helm-lsp
+;;  :ensure t
+;;  :commands helm-lsp-workspace-symbol)
