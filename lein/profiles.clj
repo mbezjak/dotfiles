3d9887ba24c4a587771fdf2994c3eff008a1c3f4
Miro Bezjak <bezjak.miro@gmail.com>
2021-09-14 11:56:25 +0200

    lein: add djblue/portal as a user dependency

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
-                  [polylith/lein-polylith "0.2.0"]]}}
+                  [polylith/lein-polylith "0.2.0"]]
+        :dependencies [[djblue/portal "0.14.0"]]}}
