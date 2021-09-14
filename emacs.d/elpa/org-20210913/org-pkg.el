0d7861e5c6c40152d5af5299876d4e813707a1de
Miro Bezjak <bezjak.miro@gmail.com>
2021-09-14 09:35:32 +0200

    emacs: update elpa

--- emacs.d/elpa/org-20210913/org-pkg.el
+++ emacs.d/elpa/org-20210913/org-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org"
-  "20210906" "Outline-based notes management and organizer" ( ))
+  "20210913" "Outline-based notes management and organizer" ( ))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
--- emacs.d/elpa/org-20210913/org-version.el
+++ emacs.d/elpa/org-20210913/org-version.el
@@ -11,7 +11,7 @@ Inserted by installing Org mode or when a release is made."
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.4.6-12-gdcc3a8-elpa"))
+   (let ((org-git-version "9.4.6-13-g4be129-elpa"))
      org-git-version))
 
 (provide 'org-version)
