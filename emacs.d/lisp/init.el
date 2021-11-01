a6a86de747d0b1af084bd6fbf0c6ea5601165486
Miro Bezjak <miro.bezjak@protonmail.com>
2021-11-01 19:02:41 +0100

    emacs: disable lock files due to create-react-app

--- emacs.d/lisp/init.el
+++ emacs.d/lisp/init.el
@@ -51,7 +51,9 @@
       dired-recursive-copies 'always
       dired-recursive-deletes 'top
       dired-listing-switches "-lha --group-directories-first"
-      dired-dwim-target t)
+      dired-dwim-target t
+      ;; node / create-react-app dislikes lock files
+      create-lockfiles nil)
 
 ;; trying out scroll setup from frontmacs
 (setq scroll-preserve-screen-position 1)
