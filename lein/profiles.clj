80e26b0b2213a73d8dcb8f5ceabda548080f76a0
Miro Bezjak <miro.bezjak@protonmail.com>
2021-11-30 10:26:09 +0100

    lein: update deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[lein-pprint "1.2.0"]]}
- :polylith {:plugins [[polylith/lein-polylith "0.2.0"]]}
- :portal {:dependencies [[djblue/portal "0.14.0"]]}}
+{:user {:plugins [[lein-pprint "1.3.2"]]}
+ :polylith {:plugins [[polylith/lein-polylith "0.6"]]}
+ :portal {:dependencies [[djblue/portal "0.18.0"]]}}
