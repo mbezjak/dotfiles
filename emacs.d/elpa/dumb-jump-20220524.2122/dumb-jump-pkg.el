62b1648c5e16f5f47f27ffb79d348c6fd77d4749
Miro Bezjak <miro.bezjak@protonmail.com>
2022-06-03 11:59:51 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/dumb-jump-20220524.2122/dumb-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from dumb-jump.el  -*- no-byte-compile: t -*-
+(define-package "dumb-jump" "20220524.2122" "Jump to definition for 50+ languages without configuration" '((emacs "24.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :stars nil :commit "1dd583011f4025b1b8c75fd785691851b6c5dfa3" :authors '(("jack angers and contributors")) :maintainer '("jack angers and contributors") :keywords '("programming") :url "https://github.com/jacktasia/dumb-jump")
--- emacs.d/elpa/web-mode-17/web-mode-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "web-mode" "../../../../../.emacs.d/elpa/web-mode-17/web-mode.el"
-;;;;;;  "9bc5594e9bee72f0b7b253684608aa9b")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/web-mode-17/web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "web-mode" "../../../../../.emacs.d/elpa/web-mode-17/web-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/web-mode-17/web-mode.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode" '("web-mode-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/web-mode-17/web-mode-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/web-mode-17/web-mode.el") (0
-;;;;;;  0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; web-mode-autoloads.el ends here
