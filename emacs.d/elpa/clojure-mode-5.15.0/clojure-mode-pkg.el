59f712c78247ea020f9148c47d072fd2e9b6f173
Miro Bezjak <miro.bezjak@protonmail.com>
2022-07-20 10:50:34 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/clojure-mode-5.15.0/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from clojure-mode.el  -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "5.15.0" "Major mode for Clojure code" '((emacs "25.1")) :stars nil :commit "93bb1c5a2a4699063e4f2ac7e585f5cab5dd52c1" :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
---
+++ emacs.d/elpa/org-roam-ui-20220713.1144/org-roam-ui-pkg.el
@@ -0,0 +1,15 @@
+(define-package "org-roam-ui" "20220713.1144" "User Interface for Org-roam"
+  '((emacs "27.1")
+    (org-roam "2.0.0")
+    (simple-httpd "20191103.1446")
+    (websocket "1.13"))
+  :commit "2cd93bfa517d51b4c97d42817713ca5ce61cb0df" :authors
+  '(("Kirill Rogovoy, Thomas Jorna"))
+  :maintainer
+  '("Kirill Rogovoy, Thomas Jorna")
+  :keywords
+  '("files" "outlines")
+  :url "https://github.com/org-roam/org-roam-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
