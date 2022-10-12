15898264b61fa97f15d0945742ecea8fb8652881
Miro Bezjak <miro.bezjak@protonmail.com>
2022-10-12 09:59:12 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/emacsql-3.1.0/emacsql-pkg.el
@@ -0,0 +1,10 @@
+(define-package "emacsql" "3.1.0" "High-level SQL database front-end"
+  '((emacs "25.1"))
+  :commit "40c41d0d978dd53d56d3aa38fd532ddcb5802e21" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :url "https://github.com/magit/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/emacsql-sqlite-3.1.0/emacsql-sqlite-pkg.el
@@ -0,0 +1,11 @@
+(define-package "emacsql-sqlite" "3.1.0" "EmacSQL back-end for SQLite"
+  '((emacs "25.1")
+    (emacsql "3.0.0"))
+  :commit "40c41d0d978dd53d56d3aa38fd532ddcb5802e21" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :url "https://github.com/magit/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
