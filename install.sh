f60fe2c0c47202cec3e2daf01083a3bc866acd87
Miro Bezjak <miro.bezjak@protonmail.com>
2023-05-13 00:14:22 +0200

    install: Remove broken link

--- install.sh
+++ install.sh
@@ -22,14 +22,10 @@ mkdir --parents --mode=700 ~/Sync
 mkdir --parents ~/workspace/{xattic,xforeign}
 mkdir --parents ~/.lein
 mkdir --parents ~/.clojure
-mkdir --parents ~/.sbt/0.13/plugins
-mkdir --parents ~/.config/i3
 
 install_link emacs.d .emacs.d
 install_link lein/profiles.clj .lein/profiles.clj
 install_link clojure/deps.edn .clojure/deps.edn
-install_link sbt/0.13/plugins/plugins.sbt .sbt/0.13/plugins/plugins.sbt
-install_link i3/config .config/i3/config
 install_link config/systemd/user/dump-package-list.service .config/systemd/user/dump-package-list.service
 install_link config/systemd/user/dump-package-list.timer .config/systemd/user/dump-package-list.timer
 systemctl --user enable dump-package-list.service dump-package-list.timer
@@ -42,11 +38,6 @@ for f in $(find -mindepth 1 -maxdepth 1 -type f \
                 -printf '%f\n'); do
     install_link "$f" ".$f"
 done
-
-
-if [[ "$1" == work ]]; then
-    install_link hgrc.work .hgrc.local
-fi
 
 # remove dead symbolic links
 find ~/bin -type l ! -exec test -r {} \; -exec rm -i {} \;
