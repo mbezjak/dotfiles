986918dfa6f413fbc9fe2251a35618ef14e39d84
Miro Bezjak <miro.bezjak@protonmail.com>
2023-07-14 16:12:34 +0200

    emacs: Function to configure emacs for showing code via screen sharing

--- emacs.d/lisp/my-functions.el
+++ emacs.d/lisp/my-functions.el
@@ -263,4 +263,18 @@ Saves to a temp file and puts the filename in the kill ring."
       (lispy-oneline)
       (forward-to-indentation))))
 
+(defun my-present ()
+  (interactive)
+  (setq-local face-remapping-alist '((default (:height 1.5) default)
+                                     (linum (:foreground "#ff6c6b") default)
+                                     (hl-line (:background "RosyBrown4") default)))
+  (global-hl-line-mode 1)
+  (global-linum-mode 1))
+
+(defun my-unpresent ()
+  (interactive)
+  (setq-local face-remapping-alist nil)
+  (global-hl-line-mode -1)
+  (global-linum-mode -1))
+
 (provide 'my-functions)
