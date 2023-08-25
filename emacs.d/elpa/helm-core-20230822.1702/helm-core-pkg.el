728ad7256c94c4c92f65d1914f5ee7d1b1b6468c
Miro Bezjak <miro.bezjak@protonmail.com>
2023-08-25 18:32:07 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/helm-core-20230822.1702/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20230822.1702" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "3fd54e79d5cc0cdb26bd4ac4728e1cf9d80e8896" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
