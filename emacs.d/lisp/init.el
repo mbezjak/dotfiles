436c2a01c7a1c066fbe8419e220c77077199bf53
Miro Bezjak <miro.bezjak@protonmail.com>
2023-09-06 16:49:32 +0200

    emacs: Use helpful to the fullest

--- emacs.d/lisp/init.el
+++ emacs.d/lisp/init.el
@@ -488,7 +488,10 @@
 (use-package helpful
   :ensure t
   :bind (("<f1>" . helpful-at-point)
-         ("C-h f" . helpful-function)))
+         ("C-h f" . helpful-callable)
+         ("C-h v" . helpful-variable)
+         ("C-h k" . helpful-key)
+         ("C-h x" . helpful-command)))
 
 (use-package lispy
   :ensure t
