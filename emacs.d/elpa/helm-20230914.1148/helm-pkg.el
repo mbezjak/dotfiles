2c7c52157fd57562c91de94730d8066e9b0005db
Miro Bezjak <miro.bezjak@protonmail.com>
2023-09-19 15:44:05 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/helm-20230914.1148/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20230914.1148" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.4")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "880d8a76d73f3df1364208e2a195185f6fa4c7ad" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/helm-core-20230914.1855/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20230914.1855" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "9a5827284a63453156511b935de39180f4f52d31" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
