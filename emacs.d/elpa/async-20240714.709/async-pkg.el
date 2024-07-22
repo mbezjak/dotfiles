89d723d60512277322db9703715b5a83c8dc0bd4
Miro Bezjak <miro.bezjak@protonmail.com>
2024-07-22 09:58:53 +0200

    emacs: Update elpa

---
+++ emacs.d/elpa/async-20240714.709/async-pkg.el
@@ -0,0 +1,14 @@
+(define-package "async" "20240714.709" "Asynchronous processing in Emacs"
+  '((emacs "24.4"))
+  :commit "3623cb328a8d810d3277de6f005cce18fbcd8b1e" :authors
+  '(("John Wiegley" . "jwiegley@gmail.com"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :keywords
+  '("async")
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/company-20240714.29/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20240714.29" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "c67946d6e32060a046543411ce93d1656c2c882c" :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
