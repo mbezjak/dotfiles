ada974f75cb29c9a25de2699e917cf7e48ab3575
Miro Bezjak <miro.bezjak@protonmail.com>
2024-10-30 09:18:38 +0100

    emacs: Update elpa

---
+++ emacs.d/elpa/helm-ag-20241028.144/helm-ag-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-ag" "20241028.144"
+  "The silver searcher with helm interface."
+  '((emacs "27.1")
+    (helm  "2.0"))
+  :url "https://github.com/emacsorphanage/helm-ag"
+  :commit "01d36d6eb689305ea3aae0a23aa8be0029f0410f"
+  :revdesc "01d36d6eb689"
+  :authors '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainers '(("Jen-Chieh Shen" . "jcs090218@gmail.com")))
