088464805d44b167e45138839635044dec69d64f
Miro Bezjak <miro.bezjak@protonmail.com>
2024-12-02 11:07:45 +0100

    lein: Always add tools.namespace because lispy requires it

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-pprint "1.3.2"]]}
+{:user {:plugins [[lein-pprint "1.3.2"]]
+        :dependencies [[org.clojure/tools.namespace "1.5.0"]]}
  :polylith {:plugins [[polylith/lein-polylith "0.6"]]}
  :portal {:dependencies [[djblue/portal "0.18.0"]]}}
