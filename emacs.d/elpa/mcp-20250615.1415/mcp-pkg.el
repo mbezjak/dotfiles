0f02ec231b51ba4b6bcc4ba98557855a6c6072e4
Miro Bezjak <miro.bezjak@protonmail.com>
2025-07-14 16:18:40 +0200

    emacs: Latest packages

---
+++ emacs.d/elpa/mcp-20250615.1415/mcp-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "mcp" "20250615.1415"
+  "Model Context Protocol."
+  '((emacs   "30.1")
+    (jsonrpc "1.0.25"))
+  :url "https://github.com/lizqwerscott/mcp.el"
+  :commit "5288951946664271ded8faa26aed72ed2f2f0b64"
+  :revdesc "528895194666"
+  :keywords '("tools")
+  :authors '(("lizqwer scott" . "lizqwerscott@gmail.com"))
+  :maintainers '(("lizqwer scott" . "lizqwerscott@gmail.com")))
